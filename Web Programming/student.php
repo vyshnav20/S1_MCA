fb4b99b8485a8a754428ede3408d067097b556c6
vyshnav <vyshnavmohan20@gmail.com>
2023-11-23 02:09:57 +0530

    22/11/23

--- Web Programming/student.php
+++ Web Programming/student.php
@@ -1,32 +1,22 @@
-<html>
-<head>
-    <title>Student Registration</title>
-</head>
-<body>
-    <center><h1>Register</h1>
-    <form action="student.php" method="post">
-        Name: <input type="text" placeholder="Enter your name" name="name"><br>
-        Roll No:<input type="text" placeholder="Enter Roll Number" name="rno"><br>
-        Age:<input type="text" placeholder="Enter your Age" name="age"><br>
-        <input type="submit">
-    </form>    
-</center>
-</body>
-</html>
 <?php
-$conn=mysqli_connect("localhost","root","cetmca","mca_s1");
+$conn=mysqli_connect("localhost","root","cetmca","student");
 $name=$_POST['name'];
-$rno=(int)$_POST['rno'];
-$age=(int)$_POST['age'];
+$rno=$_POST['rno'];
 if(!$conn)
     echo mysqli_connect_error();
 else
 {
-    echo "hi";
-    $q="insert into student values($name,$rno,$age)";
-    if(mysqli_query($conn,$q))
+    $q= "insert into stud values(12,$name,75)";
+    $p= mysqli_query($conn,$q);
+    if($p)
     {
         echo "Inserted";
     }
+    else
+    {
+        echo mysqli_error($conn);
+    }
+    
 }
+
 ?>
