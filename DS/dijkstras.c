9de3a9eb8d0f6cd1e2f739d3dec1803796430f02
Vyshnav <vyshnavmohan20@gmail.com>
2023-12-03 20:36:42 +0530

    3/12/23

--- DS/dijkstras.c
+++ DS/dijkstras.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-int c = 0;
 void fn(int n, int A[n][n], int V[], int D[], int start, int u)
 {
+    
     V[start] = 1;
+    V[u]=1;
     D[start] = 0;
     for (int i = 0; i < n; i++)
     {
         if (A[u][i] != 999)
         {
-            V[i] = 1;
+            
             if ((D[u] + A[u][i]) < D[i])
                 D[i] = D[u] + A[u][i];
         }
     }
-    c++;
-    if (c != 2)
-        fn(n, A, V, D, start, 1);
-}
+    int min =999;
+    int pos=-1;
+    for (int i=0; i<n;i++)
+    {
+        if(V[i]==0 && D[i]<min)
+        {
+            min=D[i];
+            pos=i;
+        }
+    }
+    if(pos!=-1)
+        fn(n, A, V, D, start, pos);
+            
+ }
+        
+    
+        
 
 void main()
 {
@@ -44,6 +58,4 @@ void main()
     fn(n, A, V, D, start, start);
     for (int i = 0; i < n; i++)
         printf("%d ", D[i]);
-    for (int i = 0; i < n; i++)
-        printf("%d ", V[i]);
 }
