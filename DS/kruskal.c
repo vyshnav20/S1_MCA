210a61c8fc6d77c2dafe7fc3cc8c2f127045ef15
Vyshnav <vyshnavmohan20@gmail.com>
2023-12-10 15:58:16 +0530

    10/12/23

--- DS/kruskal.c
+++ DS/kruskal.c
@@ -1,20 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+int MAX = 10, rear = -1, front = -1;
+int a[10];
+void Enqueue(int n)
+{
+    int val;
+    
+    if (rear == MAX - 1)
+        return;
+    if (rear == -1)
+            front = 0;
+    rear++;
+    a[rear] = n;
+    }
+
+
+int Dequeue()
+{
+    if (front == -1)
+        return -1;
+    int v = a[front];
+    front++;
+    return v;
+
+}
+
+int cycle(int u,int v,int n,int a[n][n])
+{
+    int v1[n];
+    for (int i = 0; i < n; i++)
+        v1[i] = 0;
+    int start=u,c=0;
+    Enqueue(start);
+    v1[start]=1;
+    while(front<=rear)
+    {
+        start=Dequeue();
+        for(int i=0; i<n;i++)
+            if(a[start][i]==1 && v1[i]==0)
+            {
+                Enqueue(i);
+                v1[i]=1;
+                if(i==v)
+                {
+                    c=1;
+                    break;
+                }
+            }
+    }
+    if(c==1)
+        return 1;
+    else
+        return 0;
+
+}
+
 void main()
 {
     int n;
     printf("Enter number of nodes: ");
     scanf("%d", &n);
-    int A[n][n], V[n], N[n], cost = 0, u, v, min = 999, e = 1;
+    int A[n][n], a[n][n],V[n], N[n], cost = 0, u, v, min = 999, e = 1;
     for (int i = 0; i < n; i++)
     {
         V[i] = 0;
-        N[i] = i + 1;
+        N[i] = i;
     }
     printf("Enter the cost adjacency matrix of the graph: ");
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
         {
+            
             scanf("%d", &A[i][j]);
             if (A[i][j] == 0)
                 A[i][j] = 999;
@@ -24,13 +80,16 @@ void main()
                 u = i;
                 v = j;
             }
+            a[i][j]=0;
         }
-    V[u] = 1;
-    V[v] = 1;
     cost += A[u][v];
     A[u][v] = 999;
+    A[v][u] = 999;
+    a[u][v]=1;
+    a[v][u]=1;
+    printf("\n Minimum cost spanning tree:\n");
     printf("Edge {%d,%d} = %d \n", N[u], N[v], min);
-    while (e < n - 1)
+    while (e <n-1)
     {
         min = 999;
         for (int i = 0; i < n; i++)
@@ -39,21 +98,32 @@ void main()
             {
                 if (A[i][j] < min)
                 {
-                    if (V[j] != 1)
-                    {
+                    
                         min = A[i][j];
                         u = i;
                         v = j;
-                    }
+                    
                 }
             }
         }
-        cost += min;
-        A[u][v] = 999;
-        e += 1;
-        V[v] = 1;
-        V[u] = 1;
-        printf("Edge {%d,%d} = %d \n", N[u], N[v], min);
+        if(cycle(u,v,n,a)==0)
+        {
+            cost += min;
+            A[u][v] = 999;
+            A[v][u] = 999;
+            e += 1;
+            printf("Edge {%d,%d} = %d \n", N[u], N[v], min);
+            a[u][v]=1;
+            a[v][u]=1;
+            front=-1;
+            rear=-1;
+        }
+        else{
+            A[u][v] = 999;
+            A[v][u] = 999;            
+            front=-1;
+            rear=-1;
+        }
     }
     printf("Minimum Cost: %d\n", cost);
 }
