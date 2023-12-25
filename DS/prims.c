47d17b54064f07aa4135db82cb3c999af8fd9529
Vyshnav <vyshnavmohan20@gmail.com>
2023-12-25 12:08:27 +0530

    25/12/23

--- DS/prims.c
+++ DS/prims.c
@@ -1,57 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 void main()
 {
-    int n;
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
-    int A[n][n], V[n], N[n], cost = 0, u, v, min = 999, e = 1;
-    for (int i = 0; i < n; i++)
-    {
-        V[i] = 0;
-        N[i] = i;
-    }
-    printf("Enter the cost adjacency matrix of the graph: ");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-        {
-            scanf("%d", &A[i][j]);
-            if (A[i][j] == 0)
-                A[i][j] = 999;
-            if (A[i][j] < min)
-            {
-                min = A[i][j];
-                u = i;
-                v = j;
-            }
-        }
-    V[u] = 1;
-    V[v] = 1;
-    cost += A[u][v];
-    printf("Edge {%d,%d} = %d \n", N[u], N[v], min);
-    while (e < n - 1)
-    {
-        min = 999;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (V[i] == 1)
-                {
-                    if (A[i][j] < min && V[j] != 1)
-                    {
-                            min = A[i][j];
-                            u = i;
-                            v = j;                        
-                    }
-                }
-            }
-        }
-        cost += min;
-        e += 1;
-        V[v] = 1;
-        printf("Minimum Cost Spanning Tree\n");
-        printf("Edge {%d,%d} = %d \n", N[u], N[v], min);
-    }
-    printf("Minimum Cost: %d\n", cost);
+	int n;
+	printf("Enter no:of vertices: ");
+	scanf("%d",&n);
+	int A[n][n],vi[n],u,v,min=999,cost=0,e=1;
+	printf("Enter adjacency matrix: ");
+	for(int i=0;i<n;i++)
+	{
+		vi[i]=0;
+		for(int j=0;j<n;j++)
+		{
+			scanf("%d",&A[i][j]);
+			if(A[i][j]==0)
+				A[i][j]=999;
+			if(A[i][j]<min)
+			{
+				min=A[i][j];
+				u=i;
+				v=j;
+			}
+		}
+	}
+	cost+=min;
+	vi[u]=1;
+	vi[v]=1;
+	printf("\nSpanning Tree\n");
+	printf("Edge {%d,%d}= %d\n",u,v,min);
+	while(e<n-1)
+	{
+		min=999;
+		for(int i=0;i<n;i++)
+		{
+			if(vi[i]==1)
+			{
+				for(int j=0;j<n;j++)
+				{
+					if(A[i][j]<min && vi[j]==0)
+					{
+						min=A[i][j];
+						u=i;
+						v=j;
+					}
+				}
+			}
+		}
+		cost+=min;
+		vi[v]=1;
+		printf("Edge {%d,%d}= %d\n",u,v,min);
+		e+=1;
+	}
+	printf("Cost = %d",cost);
 }
